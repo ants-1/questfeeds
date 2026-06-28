13534b0c50e553ecb90396f768c38324d4cb42ba
Anthony <anthonysalisbury98@outlook.com>
2026-06-28 17:54:07 +0100

    chore setup MongoDB and Mongoose

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,12 +3,15 @@ import { Server } from "socket.io";
 
 import app from "./app";
 import config from "./config/serverConfig";
+import connectDB from "./config/db";
 import { initialiseSocket } from "./socket";
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {});
 
 initialiseSocket(io);
+
+connectDB();
 
 httpServer.listen(config.port, () => {
   console.log(`Server running on port ${config.port}`);
